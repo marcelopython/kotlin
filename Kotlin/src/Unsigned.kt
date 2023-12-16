6e8816682f60f65444ebd7565210c94409fbc31d
Marcelo <msr88957@gmail.com>
2023-12-16 08:56:37 -0300

    variaveis mutavel e não mutavel

---
+++ Kotlin/src/Unsigned.kt
@@ -0,0 +1,23 @@
+
+// enter pointer
+fun main(){
+
+    // O minimo e 0
+    println("ULong MAX - ${ULong.MAX_VALUE} - ${ULong.MIN_VALUE}")
+    println("UInt MAX - ${UInt.MAX_VALUE} - ${UInt.MIN_VALUE}")
+    println("UShort MAX - ${UShort.MAX_VALUE} - ${UShort.MIN_VALUE}")
+    println("UByte MAX - ${UByte.MAX_VALUE} - ${UByte.MIN_VALUE}")
+
+    var abc: UInt = 0u
+    var short: UShort = 10u
+    var byte: UByte = 10u
+    var long: ULong = 10u
+
+    var num: Int = 10
+    println(num - 10 )
+
+    println(long)
+
+}
+
+
---
+++ Kotlin/src/VariaveisMutaveisENaoMutaveis.kt
@@ -0,0 +1,16 @@
+
+// enter pointer
+fun main(){
+    //Mutavel
+//    var nome = "Tony"
+//    nome = "Stark"
+//
+//    var ano = 2050
+//    ano = 2051
+
+    // Da erro
+//    val  nome = "Tony"
+//    nome = "Stark"
+}
+
+
